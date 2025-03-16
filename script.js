ec466462cbc0d798a42bdb19f8ea8d15f88c1cdd
lizakasam <lizakasam07@gmail.com>
2025-03-16 14:57:26 +0530

    Added banner

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 function handleScroll() {
   const scrollPosition = window.scrollY;
   const mainLogo = document.getElementById("mainLogo");
+  const bannertext = document.getElementById("bannertext");
+  const header = document.getElementById("header");
+  const cart = document.getElementById("cart");
 
   if (scrollPosition > 20) {
     mainLogo.classList.add("header-mode");
+    bannertext.classList.add("transparent");
+    header.classList.add("opaque");
+    cart.classList.remove("transparent");
   } else {
     mainLogo.classList.remove("header-mode");
+    bannertext.classList.remove("transparent");
+    header.classList.remove("opaque");
+    cart.classList.add("transparent");
   }
 }
 
